4fe0e6429eee02bf7257704eb547a50459f9bbba
Adrian She <adrian.she@alumni.ubc.ca>
2015-08-25 16:47:44 -0400

    Version to be hosted

--- UI.R
+++ UI.R
@@ -8,7 +8,7 @@ shinyUI(
                           sidebarPanel(
                             selectInput("optn", label = "Search by:", choice = c("Chemical Name", "SMILES String")),
                             textInput("string", label = h4("Input a Chemical Identifer")),
-                            checkboxGroupInput("studies", label = h4("Studies"), list("CCLE", "CGP", "GRAY", "GNE", "GSK", "CTRP", "NCI60")),
+                            checkboxGroupInput("studies", label = h4("Studies"), list("CCLE", "CGP", "GRAY", "GNE", "GSK", "CTRP", "NCI60", "CMAP")),
                             sliderInput("cutoff", label = h4("Cutoff in %"), min = 0, max = 100, value = 50),
                             checkboxInput("row", label = "Display one row per drug", value = FALSE),
                             checkboxInput("smiles", label = "Display SMILES", value = FALSE),
@@ -18,7 +18,7 @@ shinyUI(
                           ),
                           mainPanel(
                             p("Search for similar drugs in large pharmacogenomic studies given an input drug name or SMILES id."),
-                            p("Current studies include CCLE, CGP, GRAY, GNE, GSK, CTRP, and NCI60."),
+                            p("Current studies include CCLE, CGP, GRAY, GNE, GSK, CTRP, CMAP, and NCI60."),
                             p("Similarity of two drugs is defined by the Tanimoto similarity of their extended fingerprints, as computed using rcdk package."),
                             p("Enter a query drug and press submit to complete a search. By default, an input drug will be searched against drugs in all studies."),
                             textOutput("params"),
@@ -27,29 +27,6 @@ shinyUI(
                             downloadLink('downloadResults', 'Export results to CSV')
                           )
                         )
-                      )),
-             tabPanel("Custom Similarity Search",
-                      fluidPage(
-                        sidebarLayout(
-                          sidebarPanel(
-                            selectInput("optn", label = "Search by:", choice = c("Chemical Name", "SMILES String")),
-                            uiOutput("prompt"),
-                            p("Enter a list of chemical identifers, each of the same kind and on a new line."),
-                            h3("List A"),
-                            tags$textarea(id="ListA", cols=40, rows=3),
-                            h3("List B"),
-                            tags$textarea(id="ListB", cols=40, rows=3),
-                            checkboxInput("self", label = "Compare all drugs in List A against themselves", value = FALSE),
-                            selectInput("choice", label = h3("Fingerprint"), choice = list("Extended", "Standard" , "Graph" , "PubChem"), selected = "Extended"),
-                            actionButton("compute", "Submit!")
-                            ),
-                          mainPanel(
-                            p("Returns the drug similarity matrix of all drugs identified in list A against all drugs identified in list B."),
-                            p("Similarity of drugs is the Tanimoto similarity of their fingerprints, according to the users' choice."),
-                            h3("Results"),
-                            tableOutput("Results")
-                            )
-                          )
-                        ))
+                      ))
              ))
 
---
+++ getObj.R
@@ -0,0 +1,27 @@
+library(RMySQL)
+library(rcdk)
+source("chemicalTransUtils.R")
+
+## Script to initialize application objects by
+## getting drug curation, drug fingerprints, and
+## chembl ids
+
+## Remove na values for smiles to get fingerprints
+## Create CSV with SELECT DRUG_STRUCTURE_METADATA.drug_id, smiles, inchikey, cid, study_name FROM DRUG_CURATION INNER JOIN
+## DRUG_STRUCTURE_METADATA ON DRUG_CURATION.drug_id = DRUG_STRUCTURE_METADATA.drug_id
+## INNER JOIN STUDIES ON STUDIES.study_id = DRUG_CURATION.drug_study_id;
+#drugs <- read.csv("drug_sim_app_obj.csv", na.strings = c("NA", "list()", "NULL"))
+#drugs <- drugs[!is.na(drugs$drug_id),]
+#drugs <- drugs[!is.na(drugs$smiles) & !is.null(drugs$smiles), ]
+#target.mols <- parse.smiles(as.character(drugs[,"smiles"]))
+#target.fps <- pbapply::pblapply(target.mols, get.fingerprint, type="extended")
+#drugs$fps <- target.fps
+#save(drugs, file = "drug_dat_with_fps.RData")
+
+## Get CHEMBL IDS
+load("drug_dat_with_fps.RData")
+chembls <- pbapply::pblapply(unique(drugs$drug_id), getChEMBLfromName)
+names(chembls) <- unique(drugs$drug_id)
+chembls <- Filter(function (x) (length(x) > 0 && !is.na(x)), chembls) ## Filter ChEMBL ids which were not found
+save(chembls, file = "chembls_updated.RData")
+
